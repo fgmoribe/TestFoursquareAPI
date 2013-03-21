8019fbe87dfc6d144c36a36c383acba8719c8e52
Fernando <fgmoribe@gmail.com>
2013-03-21 17:33:05 -0300

    first commit

---
+++ TestFoursquareAPI/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestFoursquareAPI
+//
+//  Created by Fernando Moribe on 3/21/13.
+//  Copyright (c) 2013 Fernando Moribe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestFoursquareAPI/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestFoursquareAPI
+//
+//  Created by Fernando Moribe on 3/21/13.
+//  Copyright (c) 2013 Fernando Moribe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
